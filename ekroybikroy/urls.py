aab5bd012a6bfbe0aec4d80144e7c06cf63725a5
Shakil Ahmed <shakilahmmeed@gmail.com>
2020-10-08 17:56:47 +0600

    fix: search is working
    Create 0014_merge_20201129_1435.py
    
    fix: misc issues fixed
    
    fix: used whatsapp instead of instagram on footer
    
    fix: search is working

--- ekroybikroy/urls.py
+++ ekroybikroy/urls.py
@@ -3,12 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from search import views as search_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('account.urls')),
-
+    path('search/', search_views.product_search, name="product_search")
 ]
 
 if settings.DEBUG:
--- search/views.py
+++ search/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from main.models import Product, Location
 
-# Create your views here.
+
+def product_search(request):
+    query = request.GET.get('q')
+    products = Product.objects.filter(title__icontains=query)
+    cities = Location.objects.filter(type='city')
+    divisions = Location.objects.filter(type='division')
+
+    context = {
+        "products": products,
+        "cities": cities,
+        "divisions": divisions,
+        "query": query
+    }
+    return render(request, 'main/posts.html', context)
